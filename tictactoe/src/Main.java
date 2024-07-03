970dd147eee61548b7a17073d15dd79d415bfa8d
aliflazuardi <aliflazuardi250999@gmail.com>
2024-07-03 14:28:38 +0700

    finish tictactoe mvp

--- tictactoe/src/Main.java
+++ tictactoe/src/Main.java
@@ -1,18 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to TicTacToe!");
 
         Board board = new Board();
 
+        char symbol = 'X';
+        Scanner sc = new Scanner(System.in);
         // run game until finished
         while(!board.isGameFinished()){
-            board.printBoard();
-            boolean isPlaced = board.placePiece(1, 1, 'X');
+            if (symbol == 'X'){
+                symbol = 'O';
+            } else {
+                symbol = 'X';
+            }
+            System.out.printf("%c turn to place piece\n", symbol);
+            int row = sc.nextInt();
+            int col = sc.nextInt();
+            boolean isPlaced = board.placePiece(row, col, symbol);
             System.out.println(isPlaced);
-            board.printBoard();
-            isPlaced = board.placePiece(1, 1, 'O');
-            System.out.println(isPlaced);
-            board.initBoard();
             board.printBoard();
         }
     }
